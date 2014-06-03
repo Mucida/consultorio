9ac50f07e27122fe1b5c567ad0d0211db2814c1c
Mucida <lucas.mucida@gmail.com>
2014-06-03 12:53:37 -0300

    primeiro commit consultorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Consultorio::Application.config.secret_key_base = '18e3013ff48492cb32ff071ee1b93d485285e0488063b3d22034b2c1a5771a7e67963dd8093babdfd3771bd0b5eb45138496cfc66752939e7d50431073594e38'
